15f971a9dd39eae4535bf015921a1cee8f30b2cd
Jaskamal Kainth <kainthjaskamal@gmail.com>
2016-01-01 21:37:33 +0530

    python script to open my compeitive programming profiles

---
+++ open_all_compeitive_programming_websites.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+webbrowser.open_new("http://codeforces.com/profile/jaskamalkainth")
+webbrowser.open_new("https://www.codechef.com/users/j1k7_7")
+webbrowser.open_new("https://www.hackerearth.com/users/jaskamalkainth")
+webbrowser.open_new("https://www.hackerrank.com/jaskamal")
+webbrowser.open_new("https://www.spoj.com/users/jaskamalkainth")
