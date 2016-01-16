a9cabfd71665823e877291b18ded4c3fe1bc6e95
Jaskamal Kainth <kainthjaskamal@gmail.com>
2016-01-16 19:04:23 +0530

    Script selects a Youtube URL of Soothing Music at random

---
+++ Relaxation_Music_For_Stress_Relief.py
@@ -0,0 +1,7 @@
+import webbrowser
+import random
+
+relax = ["https://www.youtube.com/watch?v=TBJHN9PQYVQ" , "https://www.youtube.com/watch?v=e4Eod9ljkyA" , "https://www.youtube.com/watch?v=7jTyvm27EsE","https://www.youtube.com/watch?v=TW8Zo0NDkA8","https://www.youtube.com/watch?v=VoruRrB6WTk"]
+
+webbrowser.open_new(random.choice(relax))
+
