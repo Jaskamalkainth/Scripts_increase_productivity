294d87dc8432d3e514405ca8278599bff68e5ea2
Jaskamal Kainth <kainthjaskamal@gmail.com>
2016-01-23 18:35:54 +0530

    added more URLs

--- Relaxation_Music_For_Stress_Relief.py
+++ Relaxation_Music_For_Stress_Relief.py
@@ -1,7 +1,7 @@
 import webbrowser
 import random
 
-relax = ["https://www.youtube.com/watch?v=TBJHN9PQYVQ" , "https://www.youtube.com/watch?v=e4Eod9ljkyA" , "https://www.youtube.com/watch?v=7jTyvm27EsE","https://www.youtube.com/watch?v=TW8Zo0NDkA8","https://www.youtube.com/watch?v=VoruRrB6WTk"]
+relax = ["https://www.youtube.com/watch?v=TBJHN9PQYVQ" , "https://www.youtube.com/watch?v=e4Eod9ljkyA" , "https://www.youtube.com/watch?v=7jTyvm27EsE","https://www.youtube.com/watch?v=TW8Zo0NDkA8","https://www.youtube.com/watch?v=VoruRrB6WTk","https://www.youtube.com/watch?v=8MQMMYZ_CXk","https://www.youtube.com/watch?v=qiTs4iItDTM","https://www.youtube.com/watch?v=MMtZlX1L7rs","https://www.youtube.com/watch?v=VtqRxyp-KKw"]
 
 webbrowser.open_new(random.choice(relax))
 
