ec301e8672e0955a0e217563d91e3861281b6fd3
Jaskamal Kainth <kainthjaskamal@gmail.com>
2016-01-24 18:49:37 +0530

    Script to login in Github

---
+++ github_login.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+print "Sign in to GitHub \n"
+
+emailID = raw_input("Enter Email address: Example ( xyz@gmail.com ) \n" )
+Password = raw_input("Enter your Password : Example ( jkthescriptman ) \n")
+
+browser =webdriver.Chrome()
+browser.get("https://github.com/login")
+
+inputemail = browser.find_element_by_name('login')
+inputpass = browser.find_element_by_name('password')
+
+inputemail.send_keys(emailID)
+inputpass.send_keys(Password)
+
+
+login = browser.find_element_by_name('commit')
+
+login.submit()
+
